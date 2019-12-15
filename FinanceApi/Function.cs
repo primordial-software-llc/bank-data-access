b0fa853b54edbc656518f345391807d23598f500
Timothy Gonzalez <timg456789@yahoo.com>
2019-12-15 02:14:10 -0500

    Server side signup.

--- FinanceApi/Function.cs
+++ FinanceApi/Function.cs
@@ -68,19 +68,7 @@ namespace FinanceApi
                     user = databaseClient.Get(new FinanceUser { Email = email });
                     if (string.IsNullOrWhiteSpace(user.Email))
                     {
-                        user = new FinanceUser
-                        {
-                            Email = email,
-                            Biweekly = new List<JObject> { new JObject { { "date", "2015-12-25T00:00:00Z" }, { "amount", 0 } } },
-                            MonthlyRecurringExpenses = new List<JObject>(),
-                            WeeklyRecurringExpenses = new List<JObject>()
-                        };
-                        var update = JObject.FromObject(user, new JsonSerializer { NullValueHandling = NullValueHandling.Ignore });
-                        var dbClient = new AmazonDynamoDBClient();
-                        dbClient.PutItemAsync(
-                            new FinanceUser().GetTable(),
-                            Document.FromJson(update.ToString()).ToAttributeMap()
-                        ).Wait();
+                        throw new Exception($"User not found {email}. Use the correct signup form at https://www.primordial-software.com/pages/login-signup.html");
                     }
                 }
                 catch (Exception exception)
@@ -116,6 +104,30 @@ namespace FinanceApi
                          string.Equals(request.Path, "/unauthenticated/refreshToken", StringComparison.OrdinalIgnoreCase))
                 {
                     return new RefreshToken().Run(email, request, response);
+                }
+                else if (string.Equals(request.HttpMethod, "POST", StringComparison.OrdinalIgnoreCase) &&
+                         string.Equals(request.Path, "/unauthenticated/signup", StringComparison.OrdinalIgnoreCase))
+                {
+                    var body = JObject.Parse(request.Body);
+                    if (!body["agreedToLicense"].Value<bool>())
+                    {
+                        throw new Exception("agreedToLicense must be true");
+                    }
+                    var provider = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(), RegionEndpoint.USEast1);
+                    var userPool = new CognitoUserPool(Configuration.FINANCE_API_COGNITO_USER_POOL_ID, Configuration.FINANCE_API_COGNITO_CLIENT_ID, provider);
+                    var result = userPool.SignUpAsync(
+                        body["email"].Value<string>(),
+                        body["password"].Value<string>(),
+                        new Dictionary<string, string>(), new Dictionary<string, string>());
+                    result.Wait();
+                    CreateUser(email, body["agreedToLicense"].Value<bool>(), request.RequestContext.Identity.SourceIp);
+                    json = new JObject
+                    {
+                        { "status", "Your user has successfully been created. " +
+                                    $"Your user name is ${body["email"].Value<string>()}. " +
+                                    "A confirmation link has been sent to your email from noreply@primordial-software.com. " +
+                                    "You need to click the verification link in the email before you can login." }
+                    };
                 }
                 else if (string.Equals(request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase) &&
                          string.Equals(request.Path, "/getUsername", StringComparison.OrdinalIgnoreCase))
@@ -222,20 +234,13 @@ namespace FinanceApi
                     string.Equals(request.Path, "/budget"))
                 {
                     var jsonPatch = JObject.Parse(request.Body);
-                    if (jsonPatch["licenseAgreement"] != null &&
-                        jsonPatch["licenseAgreement"]["agreedToLicense"] != null &&
-                        jsonPatch["licenseAgreement"]["agreedToLicense"].Value<bool>() &&
-                        !(user.LicenseAgreement?.AgreedToLicense).GetValueOrDefault())
-                    {
-                        jsonPatch["licenseAgreement"]["ipAddress"] = request.RequestContext.Identity.SourceIp;
-                        jsonPatch["licenseAgreement"]["agreementDateUtc"] = DateTime.UtcNow.ToString("O");
-                    }
-                    else if (jsonPatch["licenseAgreement"] != null)
+                    if (jsonPatch["licenseAgreement"] != null)
                     {
                         jsonPatch.Remove("licenseAgreement");
                     }
                     var updateItemResponse = UpdateUser(email, jsonPatch);
                     json = JObject.Parse(Document.FromAttributeMap(updateItemResponse.Attributes).ToJson());
+                    // CAUTION: NEED TO SEE IF BANK LINKS GET RETURNED.
                     response.StatusCode = (int) updateItemResponse.HttpStatusCode;
                 }
                 else if (string.Equals(request.HttpMethod, "GET") &&
@@ -313,6 +318,30 @@ namespace FinanceApi
 
             return string.Empty;
         }
+
+        private void CreateUser(string email, bool agreedToLicense, string ip)
+        {
+            var user = new FinanceUser
+            {
+                Email = email,
+                Biweekly = new List<JObject>(),
+                MonthlyRecurringExpenses = new List<JObject>(),
+                WeeklyRecurringExpenses = new List<JObject>(),
+                LicenseAgreement = new LicenseAgreement
+                {
+                    AgreedToLicense = agreedToLicense,
+                    Date = DateTime.UtcNow.ToString("O"),
+                    IpAddress = ip
+                }
+            };
+            var update = JObject.FromObject(user, new JsonSerializer { NullValueHandling = NullValueHandling.Ignore });
+            var dbClient = new AmazonDynamoDBClient();
+            dbClient.PutItemAsync(
+                new FinanceUser().GetTable(),
+                Document.FromJson(update.ToString()).ToAttributeMap()
+            ).Wait();
+        }
+
         private UpdateItemResponse UpdateUser(string email, JObject update)
         {
             var dbClient = new AmazonDynamoDBClient();
