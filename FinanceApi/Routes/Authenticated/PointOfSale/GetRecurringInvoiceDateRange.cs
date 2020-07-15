838018f44b4922d17bb9036e177b7c405a045137
Timothy Gonzalez <timothy@primordial-software.com>
2020-07-15 13:25:53 -0400

    Add frequency url param.

--- FinanceApi/Routes/Authenticated/PointOfSale/GetRecurringInvoiceDateRange.cs
+++ FinanceApi/Routes/Authenticated/PointOfSale/GetRecurringInvoiceDateRange.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Amazon.Lambda.APIGatewayEvents;
 using FinanceApi.DatabaseModel;
-using FinanceApi.RequestModels;
 using Newtonsoft.Json;
 using PropertyRentalManagement;
 using PropertyRentalManagement.BusinessLogic;
@@ -15,12 +14,12 @@ namespace FinanceApi.Routes.Authenticated.PointOfSale
 
         public void Run(APIGatewayProxyRequest request, APIGatewayProxyResponse response, FinanceUser user)
         {
-            var model = JsonConvert.DeserializeObject<GetRecurringInvoiceDateRangeModel>(request.Body);
             var year = int.Parse(request.QueryStringParameters["year"]);
             var month = int.Parse(request.QueryStringParameters["month"]);
             var dayOfMonth = int.Parse(request.QueryStringParameters["dayOfMonth"]);
+            var frequency = (RecurringInvoices.Frequency) Enum.Parse(typeof (RecurringInvoices.Frequency), request.QueryStringParameters["frequency"]);
             var date = new DateTime(year, month, dayOfMonth, 0, 0, 0, DateTimeKind.Utc);
-            DateRange dateRange = model.Frequency == RecurringInvoices.Frequency.Weekly
+            DateRange dateRange = frequency == RecurringInvoices.Frequency.Weekly
                 ? RecurringInvoices.GetWeekDateRange(date)
                 : RecurringInvoices.GetMonthDateRange(date);
             response.Body = JsonConvert.SerializeObject(dateRange);
