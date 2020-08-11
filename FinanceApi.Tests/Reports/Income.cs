5db5add800bb2ae9cdbd92557a58725a1af2726d
Timothy Gonzalez <timothy@primordial-software.com>
2020-08-10 22:54:20 -0400

    Update report location.

--- FinanceApi.Tests/Reports/Income.cs
+++ FinanceApi.Tests/Reports/Income.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using FinanceApi.Tests.InfrastructureAsCode;
 using PropertyRentalManagement.Reports;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace FinanceApi.Tests.InfrastructureAsCode.Reports
+namespace FinanceApi.Tests.Reports
 {
     public class Income
     {
@@ -17,8 +18,8 @@ namespace FinanceApi.Tests.InfrastructureAsCode.Reports
         [Fact]
         public void PrintDailyIncome()
         {
-            var start = new DateTime(2020, 8, 2);
-            var end = new DateTime(2020, 8, 2);
+            var start = new DateTime(2020, 8, 9);
+            var end = new DateTime(2020, 8, 9);
             IncomeReport.PrintReport(
                 start,
                 end, new XUnitLogger(Output),
