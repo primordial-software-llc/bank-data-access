795b12d259328d31fabad343c43eb613e9e18c18
Timothy Gonzalez <timg456789@yahoo.com>
2021-01-06 02:45:03 -0500

    Add missing restricted areas.

--- FinanceApi.Tests/InfrastructureAsCode/PointOfSale/CreateSpotReservationTable.cs
+++ FinanceApi.Tests/InfrastructureAsCode/PointOfSale/CreateSpotReservationTable.cs
@@ -1,13 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using PropertyRentalManagement.DatabaseModel;
+using PropertyRentalManagement.DataServices;
 using Xunit;
 
 namespace FinanceApi.Tests.InfrastructureAsCode.PointOfSale
 {
     public class CreateSpotReservationTable
     {
+        [Fact]
+        public void CreateSpots()
+        {
+            var client = new DatabaseClient<Spot>(new AmazonDynamoDBClient(Factory.CreateCredentialsFromProfile(), Factory.HomeRegion));
+
+            /*
+            client.Create(new Spot
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "North Walkway",
+                Section = new Section
+                {
+                    Id = "47afac0b-67c2-4807-a88d-3ea9e1775661",
+                    Name = "Field H"
+                }
+            });
+            */
+
+            /*
+            client.Create(new Spot
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Parking",
+                Section = new Section
+                {
+                    Id = "2d2fa812-3bcb-4955-9a7c-63922e7392fa",
+                    Name = "Field I"
+                }
+            });
+            */
+            /*
+            client.Create(new Spot
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Parking",
+                Section = new Section
+                {
+                    Id = "596c8ac6-ebf9-4438-9973-4a516288d7b9",
+                    Name = "Field J"
+                }
+            });
+            */
+            /*
+            client.Create(new Spot
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Parking",
+                Section = new Section
+                {
+                    Id = "e8651c71-e5dd-4706-a18c-d2ee0e0da00c",
+                    Name = "Field K"
+                }
+            });
+            */
+            /*
+            client.Create(new Spot
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Parking",
+                Section = new Section
+                {
+                    Id = "3408b26a-b7ed-4e76-8a42-9b574181afae",
+                    Name = "Field L"
+                }
+            });
+            */
+        }
+
         //[Fact]
         public void A_Create()
         {
