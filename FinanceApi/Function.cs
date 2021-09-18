adfab88e177852b09127127098fb9cc3a431f6e0
Timothy Gonzalez <timothy@primordial-software.com>
2021-09-18 17:40:10 -0400

    Disable transactions until further notice.

--- FinanceApi/Function.cs
+++ FinanceApi/Function.cs
@@ -104,18 +104,19 @@ namespace FinanceApi
                     routes.Add(new Routes.Authenticated.PointOfSale.GetCustomerPaymentSettings());
                     routes.Add(new Routes.Authenticated.PointOfSale.GetCustomerPaymentSettingsById());
                     routes.Add(new Routes.Authenticated.PointOfSale.GetCustomerInvoices());
-                    routes.Add(new Routes.Authenticated.PointOfSale.GetUnsentTransactions());
                     routes.Add(new Routes.Authenticated.PointOfSale.PatchVendor());
                     routes.Add(new Routes.Authenticated.PointOfSale.PostSpot());
                     routes.Add(new Routes.Authenticated.PointOfSale.PatchSpot());
                     routes.Add(new Routes.Authenticated.PointOfSale.DeleteReservation());
                     routes.Add(new Routes.Authenticated.PointOfSale.PostReceipt());
-                    routes.Add(new Routes.Authenticated.PointOfSale.PostTransaction());
-                    routes.Add(new Routes.Authenticated.PointOfSale.PostSendToAccounting());
                     routes.Add(new Routes.Authenticated.PointOfSale.GetRecurringInvoiceDateRange());
                     routes.Add(new Routes.Authenticated.PointOfSale.GetLocation());
                     routes.Add(new Routes.Authenticated.PointOfSale.GetVendorLocation());
                     routes.Add(new Routes.Authenticated.PointOfSale.PostVendorLocation());
+
+                    //routes.Add(new Routes.Authenticated.PointOfSale.GetUnsentTransactions());
+                    //routes.Add(new Routes.Authenticated.PointOfSale.PostTransaction());
+                    //routes.Add(new Routes.Authenticated.PointOfSale.PostSendToAccounting());
                 }
                 if (new PointOfSaleAuthorization().IsAuthorizedForReports(user?.Email))
                 {
